a39694ee2cc3f50c3bc53d2decafedafbcff894f
marcosdary <programadordarkness2004@gmail.com>
2026-04-06 09:53:41 -0300

    Novo Setup

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const toggle = document.getElementById("theme-toggle");
+const root = document.documentElement;
+
+// Detecta preferência salva
+const savedTheme = localStorage.getItem("theme");
+
+// Detecta sistema
+const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (savedTheme) {
+    root.setAttribute("data-theme", savedTheme);
+} else if (systemDark) {
+    root.setAttribute("data-theme", "dark");
+}
+
+updateIcon();
+
+toggle.addEventListener("click", () => {
+    const current = root.getAttribute("data-theme");
+
+    if (current === "dark") {
+        root.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    } else {
+        root.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+
+    updateIcon();
+});
+
+function updateIcon() {
+    const current = root.getAttribute("data-theme");
+    toggle.textContent = current === "dark" ? "☀️" : "🌙";
+}
